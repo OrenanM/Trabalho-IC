c625a84108ae87ecd81117064c191c591ccc8cbf
Renan Morais <renan.smorais2001@gmail.com>
2024-01-15 15:32:36 -0300

    commit select

---
+++ PFL-Non-IID-GERCOM/system/flcore/servers/serveravg.py
@@ -0,0 +1,88 @@
+import time
+from flcore.clients.clientavg import clientAVG
+from flcore.servers.serverbase import Server
+from threading import Thread
+import sys
+
+
+class FedAvg(Server):
+    def __init__(self, args, times):
+        super().__init__(args, times)
+
+        # select slow clients
+        self.set_slow_clients()
+        self.set_clients(clientAVG)
+
+        print(f"\nJoin ratio / total clients: {self.join_ratio} / {self.num_clients}")
+        print("Finished creating server and clients.")
+
+        # self.load_model()
+        self.Budget = []
+
+
+    def train(self):
+        round_no_improve = 0
+        for round in range(self.global_rounds+1):
+            s_t = time.time()
+            self.selected_clients = self.select_clients()
+            self.send_models()
+
+            if round%self.eval_gap == 0:
+                print(f"\n-------------Round number: {round}-------------")
+                print("\nEvaluate global model")
+                self.evaluate()
+
+            for client in self.selected_clients:
+                client.train()
+
+            # threads = [Thread(target=client.train)
+            #            for client in self.selected_clients]
+            # [t.start() for t in threads]
+            # [t.join() for t in threads]
+
+            self.receive_models()
+            if self.dlg_eval and round%self.dlg_gap == 0:
+                self.call_dlg(round)
+            
+            self.aggregate_parameters()
+
+            self.Budget.append(time.time() - s_t)
+            print('-'*25, 'time cost', '-'*25, self.Budget[-1])
+
+            with open(f"{self.algorithm}_metrics_{self.entropy}_{self.weigths_clients}_{self.fall_tolerance}.txt", "a") as arquivo:
+                arquivo.write(f"{self.Budget[-1]} \n")
+
+            if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
+                break
+            
+            if (round > 0) :
+                if (self.rs_test_acc[round-1] + self.fall_tolerance) < (self.rs_test_acc[round]):
+                    round_no_improve = 0
+                else:
+                    round_no_improve += 1
+            
+            if (round_no_improve >= 5):
+                print(f'Early stopping no round {round}.')
+                self.send_models()
+                with open(f"{self.algorithm}_metrics_{self.entropy}_{self.weigths_clients}_{self.fall_tolerance}_exe.txt", "a") as arquivo:
+                    arquivo.write(f"{self.times}, {round}, {sys.getsizeof(self.global_model)}, {sys.getsizeof(self.global_model.parameters())} \n")
+                round_no_improve = 0
+                
+
+        print("\nBest accuracy.")
+        # self.print_(max(self.rs_test_acc), max(
+        #     self.rs_train_acc), min(self.rs_train_loss))
+        print(max(self.rs_test_acc))
+        print("\nAverage time cost per round.")
+        print(sum(self.Budget[1:])/len(self.Budget[1:]))
+
+        self.save_results()
+        self.save_global_model()
+
+        if self.num_new_clients > 0:
+            self.eval_new_clients = True
+            self.set_new_clients(clientAVG)
+            print(f"\n-------------Fine tuning round-------------")
+            print("\nEvaluate new clients")
+            self.evaluate(nnc = 1)
+            self.eval_new_clients = False
